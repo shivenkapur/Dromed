43f08537f29a8b7e5c8c8d0167e295874e5fe9ce
shivenkapur <shivenkapur@hotmail.com>
2018-11-15 17:02:45 +0800

    Update config/db/views.py

--- config/db/views.py
+++ config/db/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from db.models import Item, Order, Item_Asoc_Order
 from django.views.generic import View
 from braces import views
-
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
 from django.shortcuts import redirect
 
 import json
@@ -30,25 +31,46 @@ def order_view(request):
 	}
 	return render(request, 'db/dispatcher.html', context)
 
-
+def pdf_generation(request):
+    #buffer = io.BytesIO()
+    p = canvas.Canvas('db/shipping_label.pdf')
+    
+    # json =
+    
+    p.drawString(245, 750, 'SHIPPING LABEL')
+    p.drawString(100, 650, 'Order Number:')
+    p.drawString(100, 600, 'List of Items:')
+    p.drawString(100, 550, 'Final Destination:')
+    
+    p.showPage()
+    p.save()
+    
+    response = redirect('/dispatcher/')
+    return response
 
 class ContactSendView(views.CsrfExemptMixin, views.JsonRequestResponseMixin, View):
 	require_json = True
 	def post(self, request, *args, **kwargs):
 		global orderNo
+<<<<<<< HEAD
 		orderNo = Order.lastorder + 1
 		Order.lastorder += 1
+=======
+		orderNo = Order.lastorder +1
+		Order.lastorder+=1
+>>>>>>> new_one
 		quantity = 0
 		weight = 0
 		print(orderNo)
 		objects = json.loads(request.body)
-
+		print(objects)
 		items = Item.objects.all();
 		
 		for obj in objects:
 			item_asoc_order = Item_Asoc_Order.create(itemNo= int(obj['itemNo']), orderNo = orderNo, qty = obj['quantity'])
 			item_asoc_order.save();
 			quantity += obj['quantity']
+			weight += obj['quantity'] * obj['weight']
 
 
 		order = Order.create(orderNo = orderNo, noOfItems = quantity, weight = weight)
