e2a87f87cb7dd631afc17b58ea49c8c54a1173a2
Sourav Padhiari <padhiarisourav@gmail.com>
2018-11-16 17:48:50 +0800

    Update config/db/urls.py

--- config/db/urls.py
+++ config/db/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('order/', views.item_view, name = 'items-view'),
-    path('register/', views.reg, name = 'register'),
-    path('register/submit/', views.Submit.as_view(), name = 'submission'),
     path('order/send/', views.ContactSendView.as_view(), name = 'send'),
-    path('dispatcher/', views.order_view, name = 'disptacher'),
-    re_path(r'^dispatcher/warehouse/pdf/', views.PDF.as_view(), name = 'pdf-creation')
+    path('newWP/', views.new_WP, name = 'newWP'),
+    path('dequeueWP/', views.dequeue_WP, name = 'dequeuedWP'),
+    path('dispatcher/', views.new_D, name = 'dequeuedWP'),
+    path('warehouse/pdf/', views.pdf_generation, name = 'pdf-creation'),
+    path('newWP/change/', views.DequeueOrder.as_view(), name = 'dequeue'),
+    path('dequeueWP/complete/', views.CompleteOrder.as_view(), name = 'dequeue'),
+    path('login/', views.login, name='login')
+  
+
 ]
